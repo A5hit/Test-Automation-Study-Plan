1fefdb2c26041b7ba054f6bb5a80fded1a8cccb6
45hit <ashitsingh17@gmail.com>
2025-03-26 15:21:30 +0530

    game logic updated

--- Study Phases/Programming Fundamentals (Week 1-4)/Loops & Functions/Number_Guessing_Game.java
+++ Study Phases/Programming Fundamentals (Week 1-4)/Loops & Functions/Number_Guessing_Game.java
@@ -12,6 +12,8 @@ Create a Number Guessing Game where:
 
 Bonus Features:
 
+    - Allow multiple rounds.
+
     - Set a max limit of attempts and end the game if exceeded.
 
  */
@@ -22,25 +24,50 @@ public class Number_Guessing_Game {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Number Guessing Game!");
-        System.out.println("Guess a number between 0 and 100");
-        int randomNumber = (int) (Math.random() * 100);
-        gameLogic(randomNumber);
+        Scanner input = new Scanner(System.in);
+        Boolean playAgain = true;
+        while (playAgain) {
+            System.out.println("Guess a number between 0 and 100");
+            int randomNumber = (int) (Math.random() * 100);
+            gameLogic(randomNumber);
+            System.out.println("Play Again : Yes or No");
+            if (input.nextLine().equalsIgnoreCase("Yes")) {
+                playAgain = true;
+            } else {
+                playAgain = false;
+            }
+        }
     }
 
     public static void gameLogic(int randomNumber) {
 
-        for (int i = 1; i < 10; i++) {
-            Scanner sc = new Scanner(System.in);
-            int userGuess = sc.nextInt();
-            if (randomNumber == userGuess) {
-                System.out.println("Congratulations! You guessed the number " + randomNumber + "!");
-                break;
-            } else if (userGuess < randomNumber) {
+//        for (int i = 1; i < 10; i++) {
+//            Scanner sc = new Scanner(System.in);
+//            int userGuess = sc.nextInt();
+//            if (randomNumber == userGuess) {
+//                System.out.println("Congratulations! You guessed the number " + randomNumber + "!");
+//                break;
+//            } else if (userGuess < randomNumber) {
+//                System.out.println("Too Low");
+//            } else {
+//                System.out.println("Too High");
+//            }
+//        }
+
+        Scanner sc = new Scanner(System.in);
+        int userGuess = sc.nextInt();
+        int countAttempts = 1;
+
+        while (userGuess != randomNumber) {
+            if (userGuess < randomNumber) {
                 System.out.println("Too Low");
             } else {
                 System.out.println("Too High");
             }
+            userGuess = sc.nextInt();
+            countAttempts++;
         }
+        System.out.println("You guessed the number " + randomNumber + " with " + countAttempts + " attempts");
 
     }
 
